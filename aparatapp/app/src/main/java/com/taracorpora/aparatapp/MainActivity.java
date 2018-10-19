d413e76debb08561c8b34490a2e018af67ecfaa9
Kadek Bintang <kadekbintanga@gmail.com>
2018-10-19 23:22:46 +0800

    [BINTANG] init home activity

--- aparatapp/app/src/main/java/com/taracorpora/aparatapp/MainActivity.java
+++ aparatapp/app/src/main/java/com/taracorpora/aparatapp/MainActivity.java
@@ -1,13 +1,30 @@
 package com.taracorpora.aparatapp;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button fbButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fbButton = (Button) findViewById(R.id.buttonFacebook);
+        fbButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                submitFB();
+            }
+        });
+    }
+
+    private void submitFB() {
+        Intent intentHome = new Intent(this, HomeActivity.class);
+        startActivity(intentHome);
     }
 }
