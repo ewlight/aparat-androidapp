5d0d5d7c8553a9ef5dcdf5f23750d5d054369834
Kadek Bintang <kadekbintanga@gmail.com>
2018-10-20 18:45:21 +0800

    [BINTANG] create Group Page UI

--- aparatapp/app/src/main/java/com/taracorpora/aparatapp/GroupActivity.java
+++ aparatapp/app/src/main/java/com/taracorpora/aparatapp/GroupActivity.java
@@ -1,13 +1,57 @@
 package com.taracorpora.aparatapp;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 
 public class GroupActivity extends AppCompatActivity {
+    private ImageView groupButton;
+    private ImageView pengumumanButton;
+    private ImageView pengaturanButton;
+    private static String TAG;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_group);
+        TAG = GroupActivity.this.getClass().getSimpleName();
+        getSupportActionBar().setTitle("Group");
+        bindViewById();
+        pengaturanButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG, "pengaturan di tekan");
+        }
+        });
+
+        pengumumanButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG, "Pengumuman Di Klik");
+            }
+        });
+
+    }
+    private void viewPengaturan() {
+        Intent intentPengaturan = new Intent(this, PengaturanActivity.class);
+        startActivity(intentPengaturan);
+
+    }
+    private void viewPengumuman() {
+        Intent intentPengumuman = new Intent(this, HomeActivity.class);
+        startActivity(intentPengumuman);
+
+    }
+    private void bindViewById() {
+        groupButton = findViewById(R.id.groupLogo2);
+        groupButton.setBackgroundColor(Color.parseColor("#002171"));
+        pengumumanButton = findViewById(R.id.pengumumanLogo2);
+        pengaturanButton = findViewById(R.id.pengaturanLogo2);
+
     }
 }
+
--- aparatapp/app/src/main/java/com/taracorpora/aparatapp/HomeActivity.java
+++ aparatapp/app/src/main/java/com/taracorpora/aparatapp/HomeActivity.java
@@ -1,14 +1,60 @@
 package com.taracorpora.aparatapp;
 
+import android.content.Intent;
+import android.graphics.Color;
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.facebook.CallbackManager;
+import com.facebook.login.widget.LoginButton;
 
 public class HomeActivity extends AppCompatActivity {
+    private ImageView groupButton;
+    private ImageView pengumumanButton;
+    private ImageView pengaturanButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         getSupportActionBar().setTitle(getResources().getString(R.string.app_pengumuman));
+        bindViewById();
+        groupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                viewGroup();
+            }
+        });
+        pengaturanButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                viewPengaturan();
+            }
+        });
+    }
+    private void viewGroup()
+    {
+        Intent intentGroup = new Intent(this, GroupActivity.class);
+        startActivity(intentGroup);
+
+    }
+    private void viewPengaturan()
+    {
+        Intent intentPengaturan = new Intent (this, PengaturanActivity.class);
+        startActivity(intentPengaturan);
+
+    }
+    private void bindViewById()
+    {
+        groupButton = findViewById(R.id.groupLogo1);
+        pengumumanButton = findViewById(R.id.pengumumanLogo1);
+        pengumumanButton.setBackgroundColor(Color.parseColor("#002171"));
+        pengaturanButton = findViewById(R.id.pengaturanLogo1);
+
     }
 }
